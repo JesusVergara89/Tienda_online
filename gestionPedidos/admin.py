5b33131ff14dc1cde5934738f07e0f7864ff4c8f
Jesus Vergara <jesusmanuelv1989@gmail.com>
2024-07-08 10:29:59 -0600

    configuring admin in frond end

--- gestionPedidos/admin.py
+++ gestionPedidos/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from gestionPedidos.models import clientes,articulos,pedidos
+
+class clientes_admin(admin.ModelAdmin):
+    list_display=("nombre","direccion","telefono")
+    search_fields=("nombre","direccion","telefono")
+
+class articulos_admin(admin.ModelAdmin):
+    list_filter=("nombre","seccion","precio")
+
+class pedido_admin(admin.ModelAdmin):
+    list_display=("numero","fecha","entregado")
+    list_filter=("fecha",)
+    date_hierarchy="fecha"
+
+admin.site.register(clientes,clientes_admin)
+admin.site.register(articulos,articulos_admin)
+admin.site.register(pedidos,pedido_admin)
+
--- gestionPedidos/models.py
+++ gestionPedidos/models.py
@@ -3,9 +3,12 @@ from django.db import models
 # Create your models here.
 class clientes(models.Model):
     nombre=models.CharField(max_length=30)
-    direccion=models.CharField(max_length=50)
-    email=models.EmailField()
+    direccion=models.CharField(max_length=50, verbose_name="La dirección")
+    email=models.EmailField(blank=True, null=True)
     telefono=models.CharField(max_length=7)
+
+    def __str__(self):
+        return 'Nombre: {} '.format(self.nombre)
 
 class articulos(models.Model):
     nombre=models.CharField(max_length=30)
@@ -14,7 +17,6 @@ class articulos(models.Model):
 
     def __str__(self):
         return 'EL nombre es: {} la sección es {} y el precio es {}'.format(self.nombre,self.seccion,self.precio)
-
 
 class pedidos(models.Model):
     numero=models.IntegerField()
