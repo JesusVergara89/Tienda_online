9f9048c600625882b992774c730d9a493d55f250
Jesus Vergara <jesusmanuelv1989@gmail.com>
2024-07-08 10:49:23 -0600

    configuring form

--- gestionPedidos/views.py
+++ gestionPedidos/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def busqueda_productos(request):
+    return render(request, "Busqueda_productos.html")
+
+def buscar(request):
+    mensaje= "Articulo buscado: {}".format(request.GET["producto"])
+    return HttpResponse(mensaje)
